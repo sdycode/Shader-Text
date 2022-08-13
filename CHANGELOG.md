d1ebe5a40e72c825090e4f7bb93d3f7c5ecc8cfb
sdycode <sdycode@gmail.com>
2022-08-13 14:29:42 +0530

    Demo Image modified in readme and updated package to 0.0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,10 @@
 ## 0.0.1
 
 * TODO: Describe initial release.
+
+## 0.0.2
+* Image markddown code is modified
+
+## 0.0.3
+* Image markddown code is modified as previous images url were wrong
+
--- lib/example/main.dart
+++ lib/example/main.dart
@@ -1,31 +1,3 @@
-<!--
-This README describes the package. If you publish this package to pub.dev,
-this README's contents appear on the landing page for your package.
-
-For information about how to write a good package README, see the guide for
-[writing package pages](https://dart.dev/guides/libraries/writing-package-pages).
-
-For general information about developing packages, see the Dart guide for
-[creating packages](https://dart.dev/guides/libraries/create-library-packages)
-and the Flutter guide for
-[developing packages and plugins](https://flutter.dev/developing-packages).
--->
-
-TODO: This package is to make simple text to colorful texts using gradients, shader mask, fonts
-## Features
-
-TODO: It has features of various fonts with text shading styles
-
-## Getting started
-<h1>Shader Text</h1>
-
-TODO: Just import package and show ArtText widget
-
-## Usage
-
-TODO: it contains sample example whose image is given below.
-
-```dart
 import 'package:flutter/material.dart';
 import 'package:shader_texts/shader_texts.dart';
 
@@ -102,16 +74,3 @@ class _DemoState extends State<Demo> {
   }
 }
 
-
-```
-
-<table>
-  <tr>
-    <td><img src="https://raw.githubusercontent.com/sdycode/Shader-Text/main/ss1.jpg" alt = "" width="200"></td>
-  </tr>
-</table>
-
-
-## Additional information
-
-TODO: In future more styles are to be added time to time along with custom style option would be given for shading, shading styles and colors
